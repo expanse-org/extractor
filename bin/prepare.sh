0671f7741512e0a34ab197b2b6d74d807b4093d1
于红雨 <yuhongyu04@126.com>
2018-05-23 16:53:51 +0800

    add logrotate

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -28,3 +28,13 @@ svc -d $SVC_DIR
 rm -rf $WORK_DIR/src/*
 rm -rf $WORK_DIR/src/.[a-z]*
 rm -rf $WORK_DIR/bin/*
+
+#cron and logrotate are installed by default in ubuntu, don't check it again
+if [ ! -f /etc/logrotate.d/loopring-extractor ]; then
+    sudo cp bin/logrotate/loopring-extractor /etc/logrotate.d/loopring-extractor
+fi
+
+pgrep cron
+if [[ $? != 0 ]]; then
+    sudo /etc/init.d/cron start
+fi
